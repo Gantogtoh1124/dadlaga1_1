0a3c38967dbe9783ccaac24e2c2107aa3aa34d91
Gantogtoh1124 <gantogtohn240@gmail.com>
2025-06-02 13:38:47 +0800

    Dasgal 2 duusav

---
+++ src/pdadlaga2.java
@@ -0,0 +1,23 @@
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Greet method
+    public void greet() {
+        System.out.println("Hi, my name is " + name + " and I am " + age + " years old.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person("Bold", 25);
+        p.greet();
+    }
+}
+
